90f2c892b2fb454d460a88039817234006b2adfc
Bafna <bafna90433@gmail.com>
2026-04-30 17:56:33 +0530

    Fix CORS policy

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ const app = express();
 
 // Middleware
 app.use(helmet({ crossOriginResourcePolicy: false }));
-app.use(cors({ origin: '*', credentials: true }));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(morgan('dev'));
